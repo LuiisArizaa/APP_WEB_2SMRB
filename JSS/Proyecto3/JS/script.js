6ce1d4b62efd9668b12601d5d67abc2123744c59
laritru0401 <laritru0401@g.educaand.es>
2025-02-14 12:42:52 +0100

    commiteo del proyecto3

---
+++ JSS/Proyecto3/JS/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    const frasesFrikisInformatica = [
+        "Los programadores no tienen errores, solo experiencias inesperadas.",
+        "La programación es como montar en bicicleta: una vez que lo aprendes, nunca lo olvidas.",
+        "En la informática, el único límite es la imaginación (y la memoria RAM).",
+        "Los programadores no duermen, solo hacen pausas de ejecución.",
+        "La mejor forma de resolver un problema es dividirlo en problemas más pequeños.",
+        "Programar es como cocinar: a veces, una pizca de creatividad hace la diferencia.",
+        "En la programación, cada punto y coma cuenta.",
+        "El código es como el vino: mejora con el tiempo (y las revisiones).",
+        "La resolución de problemas es la habilidad más importante en la programación.",
+        "La informática es como una montaña rusa: emocionante y llena de giros inesperados."
+    ];
+    
+    const titulo = document.querySelector(".titulo");
+    const boton = document.querySelector(".boton");
+
+    function generarFraseAleatoria() {
+        const indiceAleatorio = Math.floor(Math.random() * frasesFrikisInformatica.length);
+        titulo.textContent = frasesFrikisInformatica[indiceAleatorio];
+    }
+    
+    boton.addEventListener("click", () => {
+        if (boton.textContent == "Empezar") {
+            generarFraseAleatoria();
+            boton.textContent = "Ver más contenido";
+        } else {
+            generarFraseAleatoria(); //Esto hace que cambia de frase cada vez que le haga click en ver mas contenido
+        }
+    });
+
+});
